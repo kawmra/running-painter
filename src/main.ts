055439412ad5cd36c08fa769f12c73f80cbc25f8
kawmra <m.kawmra@gmail.com>
2018-11-14 23:27:07 +0900

    Support touches and small clean up.

--- src/main.ts
+++ src/main.ts
@@ -36,17 +36,16 @@ Sketch.create({
     this.r = this.g = this.b = random(100, 200)
     target = { x: this.width / 2.0, y: this.height / 2.0 }
   },
-  mousemove() {
-    this.r = 255 * (this.mouse.x / this.width)
-    this.g = 255 * (this.mouse.y / this.height)
-    this.b = 255 * abs(cos(PI * this.mouse.y / this.width))
-    const mouse = { x: this.mouse.x, y: this.mouse.y }
-    const f = power(target, mouse)
-    const a = angle(target, mouse)
-    const a2 = angle2(target, mouse)
+  touchmove() {
+    const touch = { x: this.touches[0].x, y: this.touches[0].y }
+    this.r = 255 * (touch.x / this.width)
+    this.g = 255 * (touch.y / this.height)
+    this.b = 255 * abs(cos(PI * touch.y / this.width))
+    const f = power(target, touch)
+    const a = angle(target, touch)
     var dy = f * Math.sin(a)
     var dx = f * Math.cos(a)
-    if (target.x < mouse.x) {
+    if (target.x < touch.x) {
       dx *= -1;
       dy *= -1;
     }
